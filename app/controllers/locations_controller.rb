9461afafd087356ebd3b4e7a1c3955476475b125
Aditi Green <asaki444@gmail.com>
2017-08-11 00:25:43 -0400

    added a nested route to add a new location to a movie

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,21 @@ class LocationsController < ApplicationController
     def index
         @locations = current_user.locations
     end
+    
+    def new
+        @location = Location.new
+        @movie_id = params[:movie_id]
+    end
+    
+    def create
+        location = Location.new(location_params)
+        location.save
+        redirect_to movie_locations_path(location.movie)
+    end 
+
+    private
+
+    def location_params
+        params.require(:location).permit(:zip_code, :city, :movie_id, :user_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :movies, only: [:show, :index] do
     resources :locations
   end
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } 
 end
